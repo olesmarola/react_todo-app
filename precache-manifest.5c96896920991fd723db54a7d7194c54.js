715c7aa3c598d4f6b3eef61352be4b756afebf9f
Oles Marola <oles_pw@ukr.net>
2019-12-12 16:59:14 +0200

    Updates

--- precache-manifest.5c96896920991fd723db54a7d7194c54.js
+++ precache-manifest.5c96896920991fd723db54a7d7194c54.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "846a192574418b197737554b48685981",
+    "revision": "6181f20e84394fc93e268d10f9a7bd29",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
+    "revision": "c7695b7ca7864fb9b36e",
     "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_todo-app/static/js/2.3310f33a.chunk.js"
+    "revision": "2137e520f1c4bc3b58bf",
+    "url": "/react_todo-app/static/js/2.2b75a707.chunk.js"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
-    "url": "/react_todo-app/static/js/main.b6ea25b8.chunk.js"
+    "revision": "c7695b7ca7864fb9b36e",
+    "url": "/react_todo-app/static/js/main.f51474f0.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
