50c58bf219305272303ea44033b827d7f1dd7775
Oles Marola <oles_pw@ukr.net>
2019-12-13 14:22:35 +0200

    Updates

---
+++ precache-manifest.4d3877b37c470d022c809713dda02010.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0880a12f62a314985103067f8721191",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "8a91444260772075340b",
+    "url": "/react_todo-app/static/css/main.c746144f.chunk.css"
+  },
+  {
+    "revision": "2137e520f1c4bc3b58bf",
+    "url": "/react_todo-app/static/js/2.2b75a707.chunk.js"
+  },
+  {
+    "revision": "8a91444260772075340b",
+    "url": "/react_todo-app/static/js/main.887b524c.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_todo-app/precache-manifest.de561d734d92eaa8019400163192ea31.js"
+  "/react_todo-app/precache-manifest.4d3877b37c470d022c809713dda02010.js"
 );
 
 self.addEventListener('message', (event) => {
